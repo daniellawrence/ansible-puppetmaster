ad311fd053ae98101ccda2ab0701b67e70bec35a
Daniel Lawrence <dannyla@linux.com>
2014-06-15 21:28:37 +1000

    generate missing host keys

--- tests/init.sh
+++ tests/init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 ip a
+if [ ! -f /etc/ssh/ssh_host_dsa_key ];then
+ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -N ""
+ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ""
+fi
 /usr/sbin/sshd -D
